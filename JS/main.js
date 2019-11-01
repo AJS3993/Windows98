21fb5db339a49631f9f72906a8761f495b9894a7
Andrew Smith <ajs3993@gmail.com>
2019-11-01 12:04:22 -0500

    basic structure

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,19 @@
-console.log('Javascript File Connected')
+console.log('javascript file loaded')
+
+/*----- constants -----*/
+
+
+
+/*----- app's state (variables) -----*/
+
+const score = 0;
+
+/*----- cached element references -----*/
+
+
+
+/*----- event listeners -----*/
+
+
+
+/*----- functions -----*/
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-*Project 1*
+# Math Target
+
+---
+
+## Purpose
+
+Teach math to kids
+
+## User Stories
+
+As a who, I want what so that why
+
+## Wireframes
+
+https://wireframe.cc/U8pFw4
+
+## Tech
+
+html
+CSS
+Javascript
