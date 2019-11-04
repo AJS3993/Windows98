197d904b3c1c547cc2dbe5c8c377dc88533019ea
Andrew Smith <ajs3993@gmail.com>
2019-11-04 12:11:29 -0600

    user selected math functions show up

--- JS/main.js
+++ JS/main.js
@@ -12,27 +12,27 @@ const dif = 1;
 
 /*----- cached element references -----*/
 
-var plusBtn = document.querySelector('#plus');
+const plusBtn = document.querySelector('#plus');
 const minusBtn = document.querySelector('#minus');
 const multiplyBtn = document.querySelector('#multiply');
 const divideBtn = document.querySelector('#divide');
 
-const inputBox1 = document.getElementById('#input1');
-const inputBox2 = document.getElementById('#input2');
-const inputBox3 = document.getElementById('#input3');
+const inputBox1 = document.querySelector('#input1');
+const inputBox2 = document.querySelector('#input2');
+const inputBox3 = document.querySelector('#input3');
 
 /*----- event listeners -----*/
 
-plusBtn.addEventListener('click', numSelect1)
-minusBtn.addEventListener('click', numSelect1)
-multiplyBtn.addEventListener('click', numSelect1)
-divideBtn.addEventListener('click', numSelect1)
+plusBtn.addEventListener('click', signSelect1)
+minusBtn.addEventListener('click', signSelect1)
+multiplyBtn.addEventListener('click', signSelect1)
+divideBtn.addEventListener('click', signSelect1)
 
 // inputBox1.addEventListener('dragend', replace1)
 
 /*----- functions -----*/
 
-function numSelect1(){
-    const val = this.textContent
-    console.log(val)
+function signSelect1(){
+    sign = this.textContent;
+   inputBox2.textContent = sign
 }
