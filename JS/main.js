81d6647f862e17670f3e678fdb7287af390c5bde
Andrew Smith <ajs3993@gmail.com>
2019-11-04 12:53:51 -0600

    display number

--- JS/main.js
+++ JS/main.js
@@ -17,6 +17,11 @@ const minusBtn = document.querySelector('#minus');
 const multiplyBtn = document.querySelector('#multiply');
 const divideBtn = document.querySelector('#divide');
 
+const numBtn1 = document.querySelector('#num1');
+const numBtn2 = document.querySelector('#num2');
+const numBtn3 = document.querySelector('#num3');
+const numBtn4 = document.querySelector('#num4');
+
 const inputBox1 = document.querySelector('#input1');
 const inputBox2 = document.querySelector('#input2');
 const inputBox3 = document.querySelector('#input3');
@@ -28,6 +33,11 @@ minusBtn.addEventListener('click', signSelect1)
 multiplyBtn.addEventListener('click', signSelect1)
 divideBtn.addEventListener('click', signSelect1)
 
+numBtn1.addEventListener('click', numSelect1)
+numBtn2.addEventListener('click', numSelect1)
+numBtn3.addEventListener('click', numSelect1)
+numBtn4.addEventListener('click', numSelect1)
+
 // inputBox1.addEventListener('dragend', replace1)
 
 /*----- functions -----*/
@@ -35,4 +45,14 @@ divideBtn.addEventListener('click', signSelect1)
 function signSelect1(){
     sign = this.textContent;
    inputBox2.textContent = sign
+}
+
+function numSelect1(){
+    num = this.textContent;
+    if (inputBox1.textContent = ''){
+    inputBox1.textContent = num
+    }
+else { inputBox3.textContent = num
+
+}
 }
